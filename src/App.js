4246392daf0d4ee236fc46796d356acd152d2d9f
tangobeo <tangobeo@gmail.com>
2021-05-25 01:11:13 +0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import HomePage from './pages/home'
+import Header from './components/header'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Header />
+        <HomePage />
+    </div>
+  );
+}
+
+export default App;
